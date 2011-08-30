47a2b0b46b1a835605baee7475ba2ed1c16b7286
Ryan Davis <ryand-ruby@zenspider.com>
2011-08-30 14:42:11 -0700

    Added

---
+++ lib/minitest/matchers.rb
@@ -0,0 +1,3 @@
+class Minitest::Matchers
+  VERSION = "1.0.0"
+end
---
+++ test/test_minitest_matchers.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "minitest/matchers"
+
+class TestMinitest::Matchers < MiniTest::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
