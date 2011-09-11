5ed214eb85518d42f6e3180136f3477505cf1007
Wojciech Mach <wojtek@wojtekmach.pl>
2011-09-11 15:16:46 +0200

    Reorganize modules, update docs

--- lib/minitest/matchers.rb
+++ lib/minitest/matchers.rb
@@ -3,6 +3,23 @@ require "minitest/spec"
 module MiniTest::Matchers
   VERSION = "1.1.0" # :nodoc:
 
+  ##
+  # Every matcher must respond to following methods:
+  #   - #description
+  #   - #matches?
+  #   - #failure_message
+  #   - #negative_failure_message
+
+  def self.check_matcher matcher
+    [:description, :matches?, :failure_message, :negative_failure_message].each do |m|
+      unless matcher.respond_to? m
+        fail "Matcher must respond to #{m}."
+      end
+    end
+  end
+end
+
+module MiniTest
   module Assertions
     ##
     # Fails unless matcher.matches?(subject) returns true
@@ -31,8 +48,7 @@ module MiniTest::Matchers
 
   module Expectations
     ##
-    # See MiniTest::Matchers::Assertions#assert_must
-    #
+    # See MiniTest::Assertions#assert_must
     #     user.must have_valid(:email).when("user@example.com")
     #
     # :method: must
@@ -40,7 +56,7 @@ module MiniTest::Matchers
     infect_an_assertion :assert_must, :must
 
     ##
-    # See MiniTest::Matchers::Assertions#assert_wont
+    # See MiniTest::Assertions#assert_wont
     #
     #     user.wont have_valid(:email).when("bad@email")
     #
@@ -48,55 +64,6 @@ module MiniTest::Matchers
 
     infect_an_assertion :assert_wont, :wont
   end
-
-  def must &block
-    matcher = yield
-    MiniTest::Matchers.check_matcher matcher
-
-    it "must #{matcher.description}" do
-      fail "Please set subject" unless self.respond_to? :subject
-
-      assert_must matcher, subject
-    end
-
-    matcher
-  end
-
-  def wont &block
-    matcher = yield
-    MiniTest::Matchers.check_matcher matcher
-
-    it "wont #{matcher.description}" do
-      fail "Please set subject" unless self.respond_to? :subject
-
-      assert_wont matcher, subject
-    end
-
-    matcher
-  end
-
-  ##
-  # Every matcher must respond to following methods:
-  #   - #description
-  #   - #matches?
-  #   - #failure_message
-  #   - #negative_failure_message
-
-  def self.check_matcher matcher
-    [:description, :matches?, :failure_message, :negative_failure_message].each do |m|
-      unless matcher.respond_to? m
-        fail "Matcher must respond to #{m}."
-      end
-    end
-  end
-end
-
-module MiniTest::Assertions # :nodoc:
-  include MiniTest::Matchers::Assertions
-end
-
-module MiniTest::Expectations # :nodoc:
-  include MiniTest::Matchers::Expectations
 end
 
 class MiniTest::Spec # :nodoc:
